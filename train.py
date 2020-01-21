5e1bfc7a5a030f4d1ca0af6f35bd6734b4c3d63c
Amol <babaiscool@gmail.com>
2020-01-21 13:08:07 +0530

    changed no. of experiments to 50, it was taking too long in colab and it was repeatedly showing the message 'val_acc did not improve from...' after some 48th epoch

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("celebs-id")
-model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=10, num_experiments=50, enhance_data=True, batch_size=32, show_network_summary=True)
